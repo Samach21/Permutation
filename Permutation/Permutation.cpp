077671beb3e29ff21617a2290568fc7d2d709c95
Samach21 <69465282+Samach21@users.noreply.github.com>
2020-08-22 12:20:44 +0700

    50%

--- Permutation/Permutation.cpp
+++ Permutation/Permutation.cpp
@@ -1,23 +1,20 @@
 #include<stdio.h>
-#include <stdlib.h>
-
-#define MAX_VALUE 1000
 
 int fac(int);
+int loopN2(int, int);
 
 int main()
 {
-    int N, k[100000];
+    int N, k[8];
     int *n = k;
     int all;
-    int Nr;
     printf("INPUT N: ");
     if (scanf_s("%d", &N) != 1) {
         printf("ERROR\n");
         return 1;
     }
-    else if (N < 2 || N > 1000) {
-        printf("ERROR \n2 <= N <= 1000\n");
+    else if (N < 2 || N > 8) {
+        printf("ERROR \n2 <= N <= 8\n");
         return 1;
     }
     printf("INPUT ALL N: ");
@@ -34,35 +31,12 @@ int main()
     }
     all = fac(N);
     printf("ALL POSSIBLES: %d Event\n", all);
-    for (int f = 0; f < all / N; f++)
+    for (int f = 0; f < N; f++)
     {
-        for (int m = 0; m < N; m++)
+        if (N >= 2)
         {
-            
-            *n = rand() % N;
-            printf("%d ", *n);
+            loopN2(f, N);
         }
-        for (int i = 0; i < N; i++)
-        {
-            if (scanf_s("%d", &k[i]) != 1) {
-                printf("ERROR\n");
-                return 1;
-            }
-            else if (k[i] < 0 || k[i] > 9) {
-                printf("ERROR \n0 <= k <= 9\n");
-                return 1;
-            }
-        }
-        for (int l = 0; l < all; l++)
-        {
-
-        }
-        if (f == (all / N) - 1 && Nr != N)
-        {
-            f = 0;
-            Nr++;
-        }
-        printf("\n");
     }
 	return 0;
 }
@@ -75,4 +49,21 @@ int fac(int a)
         result = result * (a - i);
     }
     return result;
+}
+
+int loopN2(int re, int rN)
+{
+    int tu = 0;
+    if (rN >= 3)
+    {
+       // loopN3(re, rN);
+    }
+    else
+    {
+        for (int i = 0; i < fac(rN); i++)
+        {
+            printf("%d ", re);
+        }
+    }
+    return 0;
 }
